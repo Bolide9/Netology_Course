4822997dd903a338017a4771bc94ead71ebd64e6
Bolide9 <dgabrailov2000@mail.ru>
2021-04-30 20:46:46 +0300

    02

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-#
-#
-#
+lst = input('Введите числа: ')
+lst = lst.split()
+_lst = []
+
+for val in lst:
+    value = int(val)
+    if lst.count(str(value)) > 1:
+        _lst.append(value)
+
+_lst = list(set(_lst))
+
+print(sorted(_lst))
